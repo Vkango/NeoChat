f6f7e848c11dc35e581b3cef4b6acfac82a82ae7
Vkango <hivkan@outlook.com>
2025-01-03 23:22:22 +0800

    add chatlist(indev)

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,92 @@
 import { decrypt, encrypt } from './crypto.js';
 
 const websocket = new WebSocket('ws://127.0.0.1:1148');
-
+const message_list = {}; // 用于保存消息的对象，以群号区分
+const chatlist = {}; // 用于管理消息列表
+function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
 websocket.onopen = function() {
     console.log('WebSocket连接已建立');
 };
 
 websocket.onmessage = function(event) {
+    console.log("message_list", message_list);
+    console.log("chat_list", chatlist);
     const message = JSON.parse(event.data);
+    if (message.data && 'group_name' in message.data){
+        // WS响应获取群信息
+        chatlist[message.data.group_id].name = message.data.group_name;
+        // 更新DOM
+        const chatListContainer = document.getElementsByClassName('chat-items')[0];
+        const groupElement = document.createElement('div');
+        groupElement.className = 'chat-item';
+        groupElement.id = message.data.group_id;
+        groupElement.innerHTML = `
+                <span class="message-display">
+                    <img class="avatar" src="https://p.qlogo.cn/gh/${message.data.group_id}/${message.data.group_id}/100" alt="avatar" >
+                    <div class="username">${message.data.group_name}</div>
+                    <div class="message-">${chatlist[message.data.group_id].last_message}</div>
+                    <span class="message-time">${chatlist[message.data.group_id].send_time}</span>
+                    <span class="message-count">${chatlist[message.data.group_id].unread_count}</span>
+                </span>
+            `;
+        chatListContainer.appendChild(groupElement);
+        
+        return;
+    }
+    // 更新全局消息集
+    let group_id = message.group_id;
+    if (!message_list[group_id]) {
+        message_list[group_id] = [];
+    }
+    message_list[group_id].push({
+        text: message.message,
+        username: message.sender.card || message.sender.nickname,
+        level: message.sender.role + " | 点击开合",
+        avatar: 'https://q1.qlogo.cn/g?b=qq&nk=' + message.user_id + '&s=640',
+        raw_json: message
+    });
+
+    // 更新chatlist
+    if (!chatlist[group_id]) {
+        // 此处是一个新的群，需要获取群信息
+        // 初始化chatitem
+        chatlist[group_id] = {};
+
+        chatlist[group_id].last_message = (message.sender.card || message.sender.nickname) + ": " + message.raw_message;// TODO: 适配消息
+        chatlist[group_id].unread_count = 1; //新消息还没有读
+        chatlist[group_id].send_time = getCurrentTime();
+        // 发送WS请求获取群信息
+        websocket.send(JSON.stringify({
+            action: "get_group_info",
+            params: {
+                group_id: group_id,
+                no_cache: true
+            }
+        }));
+        // 稍后需要适配收到消息的逻辑
+    }
+    else{
+        // 已经有消息了
+        chatlist[group_id].last_message = (message.sender.card || message.sender.nickname) + ": " + message.raw_message;// TODO: 适配消息
+        chatlist[group_id].unread_count++; //新消息还没有读
+        chatlist[group_id].send_time = getCurrentTime();
+
+
+        document.getElementById(group_id).getElementsByClassName('message-')[0].innerText = chatlist[group_id].last_message;
+        document.getElementById(group_id).getElementsByClassName('message-time')[0].innerText = chatlist[group_id].send_time;
+        document.getElementById(group_id).getElementsByClassName('message-count')[0].innerText = chatlist[group_id].unread_count
+    }
+
     displayMessage({
         text: message.message,
         username: message.sender.card || message.sender.nickname,
         level: message.sender.role + " | 点击开合",
-        avatar: 'https://q1.qlogo.cn/g?b=qq&nk=' + message.user_id + '&s=640'
+        avatar: 'https://q1.qlogo.cn/g?b=qq&nk=' + message.user_id + '&s=640',
     });
 };
 
@@ -75,3 +149,15 @@ function displayMessage(message) {
 }
 
 document.getElementById('sendButton').addEventListener('click', sendMessage);
+document.addEventListener('DOMContentLoaded', function() {
+    const connectStatus = document.querySelector('.connect-status');
+    const statusBar = document.querySelector('.status-bar');
+
+    connectStatus.addEventListener('mouseenter', function() {
+        statusBar.style.display = 'block';
+    });
+
+    connectStatus.addEventListener('mouseleave', function() {
+        statusBar.style.display = 'none';
+    });
+});
